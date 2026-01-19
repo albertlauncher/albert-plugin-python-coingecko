dda8d908e6a8ceffd11118ab4511119bcc7628f0
Manuel Schneider <manuelschneid3r@gmail.com>
2026-01-19 03:11:30 +0100

    chore(release): v2.2.2

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from threading import Thread, Event
 
 md_iid = "5.0"
-md_version = "2.2.1"
+md_version = "2.2.2"
 md_name = "CoinGecko"
 md_description = "Access CoinGecko"
 md_license = "MIT"
